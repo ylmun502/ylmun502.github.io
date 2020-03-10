f07ef94869a58d0c10e5193c57bfb3f69954dd94
Yew Loon Mun <ymun2@horizon.csueastbay.edu>
2020-03-10 16:52:14 -0700

    Project 2 - completed mouse hover

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 function mouseOver(obj) {
-	obj.innerHTML = "More Baby Labrador Pictures"
+	obj.innerHTML = "<a href='https://www.akc.org/expert-advice/lifestyle/22-photos-of-labrador-retriever-puppies-to-brighten-your-day/' target='_blank'>Click Here for More Information</a>"
 }
 
 function mouseOut(obj) {
-	obj.innerHTML = "Click Here for More Information"
+	obj.innerHTML = "More Baby Labrador Pictures"
 }
+
